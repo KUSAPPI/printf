3f1f08fff70dfcd655d890dee1e00e10f7e267af
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-03-27 12:02:30 -0700

    functions.c file created

--- functions.c
+++ functions.c
@@ -3,14 +3,14 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_char - Prints a char.
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: Width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ * Return: Number of chars printed.
  */
 int print_char(va_list types, char buffer[], int flags,
 		int width, int precision, int size)
@@ -22,14 +22,14 @@ int print_char(va_list types, char buffer[], int flags,
 }
 /************************* PRINT A STRING *************************/
 /**
- * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * print_string - Prints a string.
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
  * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ * Return: Number of chars printed.
  */
 int print_string(va_list types, char buffer[], int flags,
 		int width, int precision, int size)
@@ -77,14 +77,14 @@ int print_string(va_list types, char buffer[], int flags,
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
- * print_percent - Prints a percent sign
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * print_percent - Prints a percent sign.
+ * @types: Lista of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
  * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ * Return: Number of chars printed.
  */
 int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
